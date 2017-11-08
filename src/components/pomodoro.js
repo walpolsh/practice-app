cf6c8780ac8d2cfdb882c64b7a6cd544fc195343
Paul Walsh <twentytwentyk@live.com>
2017-11-08 14:14:22 -0500

    timer fixed!

--- src/components/pomodoro.js
+++ src/components/pomodoro.js
@@ -4,15 +4,27 @@ import './pomodoro.css'
 export default class Pomodoro extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { seconds: props.seconds };
+    this.state = { seconds: parseInt(props.seconds) };
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this)
   }
-
   tick() {
     this.setState(prevState => ({
       seconds: prevState.seconds - 1
     }));
   }
 
+  increase() {
+    this.setState({
+      seconds: this.state.seconds += 60
+    });
+  }
+
+  decrease() {
+    this.setState(prevState => ({
+      seconds: prevState.seconds - 60
+    }));
+  }
   startTimer() {
     clearInterval(this.interval);
     this.interval = setInterval(() => this.tick(), 1000);
@@ -23,12 +35,13 @@ export default class Pomodoro extends React.Component {
   }
 
   render() {
+    console.log(this.state.seconds)
     let time = this.state.seconds
+    var hrs = ~~(time / 3600); //~~ substitutes Math.floor()
+    var mins = ~~((time % 3600) / 60);
+    var secs = time % 60;
     function fancyTimeFormat(time) {
           // Hours, minutes and seconds
-          var hrs = ~~(time / 3600);
-          var mins = ~~((time % 3600) / 60);
-          var secs = time % 60;
           // Output like "1:01" or "4:03:59" or "123:03:59"
           var ret = "";
           if (hrs > 0) {
@@ -46,12 +59,11 @@ export default class Pomodoro extends React.Component {
       display = <div>DONE!</div>
       button = <button onClick={() => {this.state.seconds = this.props.seconds}}>Reset</button>
 
-
     } else {
       display = <div>{fancyTimeFormat(time)}</div>
       button = <button onClick={this.startTimer.bind(this)}>Start</button>
-      plus = <button>+</button>
-      minus = <button>-</button>
+      plus = <button onClick={this.increase}>+</button>
+      minus = <button onClick={this.decrease}>-</button>
     }
     return (
       <div>
